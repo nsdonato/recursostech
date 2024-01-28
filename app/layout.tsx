a3e31e8523fc8b5ce3841c373e9ac01a1fa016fe
Noelia Donato <noeliadonato@gmail.com>
2024-01-28 15:37:26 +0100

    fix: margin top from sidebar and main contet from header

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       </head>
       <body>
         <Navbar />
-        <div className='grid grid-cols-1 gap-4 lg:grid-cols-6'>
+        <div className='grid grid-cols-1 gap-4 lg:grid-cols-6 mt-4'>
           <SidebarMenu className='' />
           <main className='p-4 lg:col-span-5'>{children}</main>
         </div>
