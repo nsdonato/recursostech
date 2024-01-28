322de742fb7b1afb8a23bfde2d3a0b5dd4efe460
Noelia Donato <noeliadonato@gmail.com>
2024-01-28 14:51:07 +0100

    feat: show on index contributors from github

--- components/contributors/contributors.test.tsx
+++ components/contributors/contributors.test.tsx
@@ -1,19 +1,62 @@
 import { render, screen } from '@testing-library/react'
 
-import { Contributor } from '@/lib/mdx/mdx-utils'
-
 import { Contributors } from './contributors'
 
+// HOW TO TEST ASYNC COMPONENTS: https://github.com/vercel/next.js/issues/47131
+/**
+ * @param {function} Component
+ * @param {*} props
+ * @returns {Promise<()=>JSX.Element>}
+ */
+async function resolvedComponent(Component, props) {
+  const ComponentResolved = await Component(props)
+  return () => ComponentResolved
+}
+
 describe('<Contributors />', () => {
-	test('renders correctly', () => {
-		const contributors: Contributor[] = [{ github_username: 'nsdonato' }]
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      json: () =>
+        Promise.resolve([
+          {
+            id: 1,
+            login: 'nsdonato',
+            avatar_url: 'https://example.com/avatar1.jpg',
+            html_url: 'https://example.com/profile1',
+            contributions: 0,
+          },
+        ]),
+    })
+  )
 
-		render(<Contributors contributors={contributors} />)
-		const avatar = screen.getByRole('img', {
-			name: /nsdonato/i,
-		})
+  test('renders correctly from index', async () => {
+    const AsyncRscComponent = await resolvedComponent(Contributors, {
+      isIndex: true,
+    })
+    render(<AsyncRscComponent />)
 
-		expect(screen.getByText('Colaboradores')).toBeInTheDocument()
-		expect(avatar).toBeInTheDocument()
-	})
+    const avatar = screen.getByTestId('contributor-nsdonato')
+
+    expect(screen.getByText('Colaboradores')).toBeInTheDocument()
+    expect(
+      screen.getByText('Gracias a todxs los que aportaron en este proyecto ✨')
+    ).toBeInTheDocument()
+    expect(avatar).toBeInTheDocument()
+  })
+
+  test('renders correctly from slug', async () => {
+    const AsyncRscComponent = await resolvedComponent(Contributors, {
+      isIndex: false,
+      contributors: [
+        {
+          github_username: 'vamoacodear',
+        },
+      ],
+    })
+    render(<AsyncRscComponent />)
+
+    const avatar = screen.getByTestId('contributor-vamoacodear')
+    expect(avatar).toBeInTheDocument()
+  })
 })
