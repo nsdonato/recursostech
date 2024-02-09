558474827aaf57d2c6c77f3f1194efcdbaf0618f
Noelia <noeliadonato@gmail.com>
2024-02-09 21:01:05 +0100

    refactor: do not invert filter only to those named as .no-filter.svg (#79)

--- components/video/video.tsx
+++ components/video/video.tsx
@@ -4,30 +4,30 @@ import { Figure } from '../figure/figure'
 import { Video as VideoType } from './video-types'
 
 type VideoProps = {
-	video: VideoType
+  video: VideoType
 }
 
 export const Video = ({ video }: VideoProps) => {
-	return (
-		<div
-			key={video.tooltip}
-			className='tooltip tooltip-sm'
-			data-tip={video.tooltip}>
-			<Link
-				className='flex gap-1 btn btn-sm btn-outline btn-accent'
-				href={video.url}
-				target='_blank'
-				rel='noopener noreferrer'>
-				<Figure
-					cover={{
-						src: '/socials/youtube.svg',
-						width: 30,
-						height: 30,
-					}}
-					className='youtube'
-					placeholder={video.tooltip}
-				/>
-			</Link>
-		</div>
-	)
+  return (
+    <div
+      key={video.tooltip}
+      className='tooltip tooltip-sm'
+      data-tip={video.tooltip}>
+      <Link
+        className='flex gap-1 btn btn-sm btn-outline btn-accent'
+        href={video.url}
+        target='_blank'
+        rel='noopener noreferrer'>
+        <Figure
+          cover={{
+            src: '/socials/youtube.no-invert.svg',
+            width: 30,
+            height: 30,
+          }}
+          className='youtube'
+          placeholder={video.tooltip}
+        />
+      </Link>
+    </div>
+  )
 }
