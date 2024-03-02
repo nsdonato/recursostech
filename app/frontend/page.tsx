1cf0c33eaa9b5e7a496af36983176c8561fdb58f
Noelia <noeliadonato@gmail.com>
2024-03-02 21:14:50 +0100

    feat: add codely on backend streamers (#97)

---
+++ app/frontend/page.tsx
@@ -0,0 +1,9 @@
+export default function FrontendHome() {
+  return null
+  // return (
+  //   <iframe
+  //     src='https://roadmap.sh/r/embed?id=65d45832cba7f7159fea0f7a'
+  //     className='h-screen w-full bg-transparent'
+  //     frameBorder='0'></iframe>
+  // )
+}
