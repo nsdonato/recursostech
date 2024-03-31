de8b8ecb9f734a69a908dc048e52f493a13d64ce
Noelia <noeliadonato@gmail.com>
2024-03-31 17:11:21 +0200

    improvement: algolia search anchors (#115)
    * feat: improvement algolia search, anchoring
    
    * feat: improvement algolia search, anchoring

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -7,12 +7,8 @@ type HeaderProps = {
 export const Header = ({ title, description, href }: HeaderProps) => {
   return (
     <header>
-      <h1
-        id={`${href ?? title}`}
-        className='text-5xl font-bold mb-10 underline underline-offset-8 decoration-pink-500 block'>
-        <a className='pointer-events-none' href={`#${href ?? title}`}>
-          {title}
-        </a>
+      <h1 className='text-5xl font-bold mb-10 underline underline-offset-8 decoration-pink-500 block'>
+        {title}
       </h1>
       {description.map((parrafo, index) => (
         <p key={index} className='text-balance last-of-type:pt-4'>
